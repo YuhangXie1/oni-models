cedbd72cb95b8ffe8e3d6d2718965add4fdd79da
Yuhang Xie <yuhangxie1@gmail.com>
2024-11-29 16:34:08 +0000

    working graph

--- model/critters.py
+++ model/critters.py
@@ -2,8 +2,9 @@
 
 class Critter:
 
-    def __init__(self, container, init_age = 0, wild = False, is_egg = False, is_lullabied = False):
+    def __init__(self, container, resource_dict, init_age = 0, wild = False, is_egg = False, is_lullabied = False):
         #initialising variables and conditions
+        self.resource_dict = resource_dict
         self.container = container
         self.age = init_age
         self.wild = wild
@@ -16,46 +17,50 @@ class Critter:
         self.tame_egg_time = None
         self.std_incubation_time = None
         self.lullabied_incubation_time = None
-        self.egg_time = self.tame_egg_time
-        self.incubation_time = self.std_incubation_time
 
+    def check_wild_incubation(self):
         #checking if wild/tame and incubated/not values should be used
         if self.wild:
             self.egg_time = self.wild_egg_time
+        else:
+            self.egg_time = self.tame_egg_time
+
         if self.lullabied:
             self.incubation_time = self.lullabied_incubation_time
+        else:
+            self.incubation_time = self.std_incubation_time
 
-        def age_one_cycle(self):
-            #ages by 1 cycle
-            self.age += self.age
+    def age_one_cycle(self):
+        #ages by 1 cycle
+        self.age += 1
 
-            if self.is_egg is False:
-                produce()
-                check_age()
-            else:
-                check_age_egg()
+        if self.is_egg is False:
+            self.produce(self.resource_dict)
+            self.check_age()
+        else:
+            self.check_age_egg()
 
-        def check_age(self):
-            #checks if critter needs to die, or lay egg
-            if self.age == self.lifespan:
-                die()
-            if self.age % self.egg_time == 0:
-                lay_egg()
+    def check_age(self):
+        #checks if critter needs to die, or lay egg
+        if self.age == self.lifespan:
+            self.die(self.resource_dict)
+        if self.age % self.egg_time == 0 and self.age > 0:
+            self.lay_egg()
 
-        def check_age_egg(self):
-            #check if egg has matured
-            if self.age == self.incubation_time:
-                self.age = 0
-                self.is_egg = False
+    def check_age_egg(self):
+        #check if egg has matured
+        if self.age == self.incubation_time:
+            self.age = 0
+            self.is_egg = False
 
-        def lay_egg(self):
-            pass
+    def lay_egg(self):
+        pass
 
-        def produce(self):
-            pass
+    def produce(self):
+        pass
 
-        def die(self):
-            pass
+    def die(self, resource_dict):
+        pass
 
 
 
@@ -69,13 +74,14 @@ class Bammoth(Critter):
         self.tame_egg_time = 12
         self.std_incubation_time = 40
         self.lullabied_incubation_time = 8
+        self.check_wild_incubation()
 
-        def lay_egg(self):
-            self.container.append(Bammoth(container = self.container, is_egg = True, wild = self.wild, is_lullabied = self.is_lullabied))
-        
-        def produce(self, resource_dict):
-            resource_dict["phosphorite"] += 10.67
+    def lay_egg(self):
+        self.container.append(Bammoth(resource_dict = self.resource_dict,container = self.container, is_egg = True, wild = self.wild, is_lullabied = self.lullabied))
+    
+    def produce(self, resource_dict):
+        resource_dict["phosphorite"] += 10.67
 
-        def die(self, resource_dict):
-            resource_dict["meat"] += 22400
+    def die(self, resource_dict):
+        resource_dict["meat"] += 22400
 
--- run.py
+++ run.py
@@ -1,19 +1,33 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from model.critters import Bammoth
 
 
 resource_dict = {
-    "phosphorite":0,
-    "meat":0
+    "phosphorite":0.0,
+    "meat":0.0
 }
 
 critters_list = []
 
-b = Bammoth(critters_list)
+b = Bammoth(resource_dict = resource_dict, container = critters_list)
 critters_list.append(b)
 
 print(critters_list)
 
-time = range(1,400)
-print(time)
+time = np.arange(1,201)
+population = []
+
+for t in time:
+    for critter in critters_list:
+        critter.age_one_cycle()
+    population.append(len(critters_list))
+
+def count(container):
+    critters = 0
+    eggs = 0
+
+
+plt.plot(time, population)
+plt.savefig("output.png")
